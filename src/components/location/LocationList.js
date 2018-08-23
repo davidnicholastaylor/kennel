43ce1059d6daffa45bb46de4bf10c829edefb414
davidnicholastaylor <dntaylor14@gmail.com>
2018-08-23 11:48:15 -0500

    created locations

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List</h1>
+                <h4>Nashville North Location</h4>
+                <h5>500 Puppy Way</h5>
+                <h4>Nashville South Location</h4>
+                <h5>314 Salamander Street</h5>
+            </article>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Kennel from './components/Kennel'
+
+ReactDOM.render(<Kennel />, document.getElementById('root'));
