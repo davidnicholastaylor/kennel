a5858d7e836f5efdd782a3b72e36ab34752c8e14
davidnicholastaylor <dntaylor14@gmail.com>
2018-08-23 13:11:53 -0500

    changed styling and content of app

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
 import LocationList from "./location/LocationList"  // Import LocationList component
+import App from "./../App"  // Import LocationList component
 
 
 export default class Kennel extends Component {
     render() {
         return (
             <div>
-                <h3>Student Kennels</h3>
+                <App />
                 <EmployeeList />
                 <LocationList />
             </div>
