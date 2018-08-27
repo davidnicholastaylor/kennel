569b77b1c04d11fabc32628744d40fa82011ace0
davidnicholastaylor <dntaylor14@gmail.com>
2018-08-27 12:08:21 -0500

    modularized api management

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,6 +5,11 @@ import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/OwnersList'
 import "./applicationViews.css"
+import AnimalManager from "../modules/AnimalManager"
+import EmployeeManager from "../modules/EmployeeManager"
+import LocationManager from "../modules/LocationManager"
+import OwnerManager from "../modules/OwnerManager"
+
 
 
 export default class ApplicationViews extends Component {
@@ -53,21 +58,26 @@ export default class ApplicationViews extends Component {
     }
 
     componentDidMount() {
-        const newState = {}
-
-        fetch("http://localhost:5002/animals")
-            .then(r => r.json())
-            .then(animals => newState.animals = animals)
-            .then(() => fetch("http://localhost:5002/employees")
-            .then(r => r.json()))
-            .then(employees => newState.employees = employees)
-            .then(() => fetch("http://localhost:5002/locations")
-            .then(r => r.json()))
-            .then(locations => newState.locations = locations)
-            .then(() => fetch("http://localhost:5002/owners")
-            .then(r => r.json()))
-            .then(owners => newState.owners = owners)
-            .then(() => this.setState(newState))
+        AnimalManager.getAll().then(allAnimals => {
+            this.setState({
+                animals: allAnimals
+            })
+        })
+        EmployeeManager.getAll().then(allEmployees => {
+            this.setState({
+                employees: allEmployees
+            })
+        })
+        LocationManager.getAll().then(allLocations => {
+            this.setState({
+                locations: allLocations
+            })
+        })
+        OwnerManager.getAll().then(allOwners => {
+            this.setState({
+                owners: allOwners
+            })
+        })
     }
 
 
